7e17a5fca7b8dea1c5f7cba370a2fd37df2426e4
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2023-10-24 14:34:09 +0200

    Terminado Piedra Papel Tijeras

--- PiedraPapelTijera/app/src/main/java/com/example/piedrapapeltijera/MainActivity.java
+++ PiedraPapelTijera/app/src/main/java/com/example/piedrapapeltijera/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Resources res = getResources();
+        final String[] pulsado = {""};
 
+        TextView resultado = (TextView) findViewById(R.id.resultado);
+        TextView seleccion = (TextView) findViewById(R.id.seleccion);
         ImageView imageView = (ImageView) findViewById(R.id.imagen);
         Button butonPlay = (Button) findViewById(R.id.buttonPlay);
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         ImageButton buttonPiedra = (ImageButton) findViewById(R.id.buttonPiedra);
         ImageButton buttonPapel = (ImageButton) findViewById(R.id.buttonPapel);
         ImageButton buttonTijera = (ImageButton) findViewById(R.id.buttonTijera);
@@ -32,6 +35,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                String maquinaResult = "";
+                int randNum = (int) Math.floor(Math.random()*(4-1+1)+1);
+                switch (randNum){
+                    case 1:
+                        maquinaResult = "piedra";
+                        imageView.setImageDrawable(res.getDrawable(R.drawable.piedra));
+                        break;
+                    case 2:
+                        maquinaResult = "papel";
+                        imageView.setImageDrawable(res.getDrawable(R.drawable.papel));
+                        break;
+                    case 3:
+                        maquinaResult = "tijera";
+                        imageView.setImageDrawable(res.getDrawable(R.drawable.tijera));
+                        break;
+                }
+
+                if (maquinaResult.equals(pulsado[0])){
+                    resultado.setText("Empate!!!");
+                } else if (maquinaResult.equals("tijera") && pulsado[0].equals("papel") ||
+                        maquinaResult.equals("piedra") && pulsado[0].equals("tijera") ||
+                        maquinaResult.equals("papel") && pulsado[0].equals("piedra")) {
+                    resultado.setText("Has perdido");
+                } else {
+                    resultado.setText("Has ganado!!!!");
+                }
+            }
+        });
+
+        buttonPapel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pulsado[0] = "papel";
+                seleccion.setText("Has seleccionado papel");
+            }
+        });
+
+        buttonPiedra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pulsado[0] = "piedra";
+                seleccion.setText("Has seleccionado piedra");
+            }
+        });
+
+        buttonTijera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pulsado[0] = "tijera";
+                seleccion.setText("Has seleccionado tijeras");
             }
         });
 
